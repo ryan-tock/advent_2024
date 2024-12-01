3785f2bc0111b690cfac54aae794711f6ee069d1
ryan <ryan@tocks.org>
2024-12-01 00:06:25 -0800

    Day 1 Part 2

--- day_01/src/main.rs
+++ day_01/src/main.rs
@@ -1,4 +1,5 @@
 use itertools::Itertools;
+use std::collections::HashMap;
 
 const INPUT: &str = include_str!("input.txt");
 
@@ -36,6 +37,16 @@ fn main() {
     let column1 = entries3.iter().map(|x| x.0).sorted().collect_vec();
     let column2 = entries3.iter().map(|x| x.1).sorted().collect_vec();
 
+    let mut occurrences = HashMap::new();
+    for i in column2.iter() {
+        *occurrences.entry(*i).or_insert(0) += 1;
+    }
+
+    let similarity_score = column1
+        .iter()
+        .map(|x| x * occurrences.get(x).copied().unwrap_or_default())
+        .sum::<i32>();
+
     let zipped = column1.into_iter().zip(column2.into_iter()).collect_vec();
 
     let difference = zipped.iter().map(|x| (x.0 - x.1).abs()).sum::<i32>();
@@ -50,4 +61,6 @@ fn main() {
     // xyz(bar2);
 
     println!("Part 1: {}", difference);
+
+    println!("Part 2: {}", similarity_score);
 }
