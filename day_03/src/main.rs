cfe2949ddddd0f0a2f190d290f0b7cc24b4a6200
ryan <ryan@tocks.org>
2024-12-02 22:37:52 -0700

    Day 3

---
+++ day_03/src/main.rs
@@ -0,0 +1,33 @@
+use itertools::Itertools;
+use regex::Regex;
+
+const INPUT: &str = include_str!("input.txt");
+
+fn main() {
+    let re = Regex::new(r"mul\((\d+),(\d+)\)").unwrap();
+
+    let disables = INPUT.split("don't()").collect_vec();
+    let active_code = disables[1..]
+        .iter()
+        .map(|x| x.split("do()").collect_vec()[1..].join(""))
+        .collect_vec();
+
+    let active_code = disables[0].to_owned() + &active_code.join("");
+
+    println!("{}", active_code);
+
+    let mut total: i32 = 0;
+    for (_, [num1, num2]) in re.captures_iter(INPUT).map(|captures| captures.extract()) {
+        total += num1.parse::<i32>().unwrap() * num2.parse::<i32>().unwrap()
+    }
+    println!("Part 1: {}", total);
+
+    let mut total: i32 = 0;
+    for (_, [num1, num2]) in re
+        .captures_iter(&active_code)
+        .map(|captures| captures.extract())
+    {
+        total += num1.parse::<i32>().unwrap() * num2.parse::<i32>().unwrap()
+    }
+    println!("Part 2: {}", total);
+}
