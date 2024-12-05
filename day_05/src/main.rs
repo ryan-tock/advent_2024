074c1dacc68d54805aeb2f2b6595947776811bc7
ryan <ryan@tocks.org>
2024-12-04 22:49:05 -0700

    Day 5 Part 2

--- day_05/src/main.rs
+++ day_05/src/main.rs
@@ -27,24 +27,13 @@ fn main() {
         .collect_vec();
 
     let mut valid_updates: Vec<Vec<i32>> = Vec::new();
+    let mut invalid_updates: Vec<Vec<i32>> = Vec::new();
 
     for update in updates {
-        let mut works = true;
-        for rule in rules.iter() {
-            let relevant = update
-                .iter()
-                .filter(|x| rule.0 == **x || rule.1 == **x)
-                .collect_vec();
-            if relevant.len() < 2 {
-                continue;
-            }
-            if !(*relevant[0] == rule.0 && *relevant[1] == rule.1) {
-                works = false;
-                break;
-            }
-        }
-        if works {
+        if is_valid(&update, &rules) {
             valid_updates.push(update);
+        } else {
+            invalid_updates.push(update);
         }
     }
 
@@ -54,4 +43,45 @@ fn main() {
         .sum::<i32>();
 
     println!("Part 1: {}", sum_of_middle);
+
+    for i in 0..invalid_updates.len() {
+        while !is_valid(&invalid_updates[i], &rules) {
+            for rule in rules.iter() {
+                let pos1 = invalid_updates[i].iter().position(|&x| x == rule.0);
+                let pos2 = invalid_updates[i].iter().position(|&x| x == rule.1);
+                if pos1.is_none() || pos2.is_none() {
+                    continue;
+                }
+                let pos1 = pos1.unwrap();
+                let pos2 = pos2.unwrap();
+                if pos1 > pos2 {
+                    invalid_updates[i][pos2] = rule.0;
+                    invalid_updates[i][pos1] = rule.1;
+                }
+            }
+        }
+    }
+
+    let sum_of_middle = invalid_updates
+        .iter()
+        .map(|x| x[(x.len() - 1) / 2])
+        .sum::<i32>();
+
+    println!("Part 2: {:?}", sum_of_middle);
+}
+
+fn is_valid(update: &Vec<i32>, rules: &Vec<(i32, i32)>) -> bool {
+    for rule in rules.iter() {
+        let relevant = update
+            .iter()
+            .filter(|x| rule.0 == **x || rule.1 == **x)
+            .collect_vec();
+        if relevant.len() < 2 {
+            continue;
+        }
+        if !(*relevant[0] == rule.0 && *relevant[1] == rule.1) {
+            return false;
+        }
+    }
+    true
 }
