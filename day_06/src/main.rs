cfb16fefbaad7745304f489c2161793801d93228
ryan <ryan@tocks.org>
2024-12-05 23:00:54 -0700

    Day 6 Part 2

--- day_06/src/main.rs
+++ day_06/src/main.rs
@@ -52,4 +52,41 @@ fn main() {
     let visited = visited.iter().map(|x| x.1).collect::<HashSet<_>>();
 
     println!("Part 1: {}", visited.len());
+
+    let mut loop_count = 0;
+
+    for new_obstacle_pos in visited {
+        if new_obstacle_pos == start_pos {
+            continue;
+        }
+        let mut visited: HashSet<((i32, i32), (i32, i32))> = HashSet::new();
+        let mut pos = start_pos;
+        let mut dir = start_dir;
+
+        let is_loop = loop {
+            if visited.contains(&(dir, pos)) {
+                break true;
+            }
+            visited.insert((dir, pos));
+
+            let mut newx = pos.0 + dir.0;
+            let mut newy = pos.1 + dir.1;
+            if newx < 0 || newy < 0 || newx >= grid[0].len() as i32 || newy >= grid.len() as i32 {
+                break false;
+            }
+            while grid[newy as usize][newx as usize] == '#' || (newx, newy) == new_obstacle_pos {
+                dir = (-dir.1, dir.0);
+                newx = pos.0 + dir.0;
+                newy = pos.1 + dir.1;
+            }
+
+            pos.0 += dir.0;
+            pos.1 += dir.1;
+        };
+        if is_loop {
+            loop_count += 1;
+        }
+    }
+
+    println!("Part 2: {}", loop_count);
 }
