dc86311923d1f88ac899039d75003714b68fcd52
ryan <ryan@tocks.org>
2024-12-06 22:29:06 -0700

    Day 7 Part 2

--- day_07/src/main.rs
+++ day_07/src/main.rs
@@ -51,4 +51,39 @@ fn main() {
     }
 
     println!("Part 1: {}", calibration_count);
+
+    let mut calibration_count: i64 = 0;
+
+    for line in lines.iter() {
+        let operators = line.1.len() - 1;
+
+        let mut works = false;
+
+        for state in 0usize..3usize.pow(operators as u32) {
+            let mut result = line.1[0];
+
+            for num in 1usize..=operators {
+                let operator_num = (state / 3usize.pow((num - 1) as u32)) % 3;
+                if operator_num == 0 {
+                    result += line.1[num];
+                } else if operator_num == 1 {
+                    result *= line.1[num];
+                } else {
+                    result *= 10usize.pow(line.1[num].to_string().len() as u32) as i64;
+                    result += line.1[num];
+                }
+            }
+
+            if result == line.0 {
+                works = true;
+                break;
+            }
+        }
+
+        if works {
+            calibration_count += line.0;
+        }
+    }
+
+    println!("Part 2: {}", calibration_count);
 }
