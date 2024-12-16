a4c9e0076bc0e0c4efe1e5dfb37e78899ca55a61
ryan <ryan@tocks.org>
2024-12-16 00:20:43 -0800

    Day 15 Part 2

--- day_15/src/main.rs
+++ day_15/src/main.rs
@@ -36,8 +36,8 @@ fn main() {
         }
     }
 
-    for i in instructions {
-        step(&mut board, &mut pos, i);
+    for i in instructions.iter() {
+        step_1(&mut board, &mut pos, *i);
     }
 
     let gps_score = board
@@ -51,9 +51,50 @@ fn main() {
         })
         .sum::<usize>();
     println!("Part 1: {}", gps_score);
+
+    let mut pos: (isize, isize) = (0, 0);
+
+    let mut board: Vec<Vec<char>> = vec![vec!['.'; board[0].len() * 2]; board.len()];
+    for (y, line) in sections[0].split("\n").enumerate() {
+        for (x, symbol) in line.chars().enumerate() {
+            if symbol == '@' {
+                pos = ((x * 2) as isize, y as isize);
+            }
+            if symbol == 'O' {
+                board[y][x * 2] = '[';
+                board[y][x * 2 + 1] = ']';
+            }
+            if symbol == '#' {
+                board[y][x * 2] = '#';
+                board[y][x * 2 + 1] = '#';
+            }
+        }
+    }
+    // print_board(&board, pos);
+
+    for i in instructions.iter() {
+        if can_step(&board, &pos, *i) {
+            step_2(&mut board, &pos, *i);
+            pos.0 += i.0;
+            pos.1 += i.1;
+        }
+        // print_board(&board, pos);
+    }
+
+    let gps_score = board
+        .iter()
+        .enumerate()
+        .map(|(y, line)| {
+            line.iter()
+                .enumerate()
+                .map(|(x, symbol)| if *symbol == '[' { 100 * y + x } else { 0 })
+                .sum::<usize>()
+        })
+        .sum::<usize>();
+    println!("Part 2: {}", gps_score);
 }
 
-fn step(board: &mut Vec<Vec<char>>, pos: &mut (isize, isize), dir: (isize, isize)) {
+fn step_1(board: &mut Vec<Vec<char>>, pos: &mut (isize, isize), dir: (isize, isize)) {
     let mut new_pos = (pos.0 + dir.0, pos.1 + dir.1);
 
     while board[new_pos.1 as usize][new_pos.0 as usize] == 'O' {
@@ -68,4 +109,58 @@ fn step(board: &mut Vec<Vec<char>>, pos: &mut (isize, isize), dir: (isize, isize
     pos.0 += dir.0;
     pos.1 += dir.1;
     board[pos.1 as usize][pos.0 as usize] = '.';
+}
+
+fn can_step(board: &Vec<Vec<char>>, pos: &(isize, isize), dir: (isize, isize)) -> bool {
+    let new_pos = (pos.0 + dir.0, pos.1 + dir.1);
+    if board[new_pos.1 as usize][new_pos.0 as usize] == '#' {
+        return false;
+    }
+    if board[new_pos.1 as usize][new_pos.0 as usize] == '.' {
+        return true;
+    }
+    if dir.1 == 0 {
+        return can_step(board, &new_pos, dir);
+    }
+    let additional_pos = if board[new_pos.1 as usize][new_pos.0 as usize] == '[' {
+        (new_pos.0 + 1, new_pos.1)
+    } else {
+        (new_pos.0 - 1, new_pos.1)
+    };
+
+    can_step(board, &new_pos, dir) && can_step(board, &additional_pos, dir)
+}
+fn step_2(board: &mut Vec<Vec<char>>, pos: &(isize, isize), dir: (isize, isize)) {
+    let symbol = board[pos.1 as usize][pos.0 as usize];
+    let new_pos = (pos.0 + dir.0, pos.1 + dir.1);
+    if board[new_pos.1 as usize][new_pos.0 as usize] == '.' {
+        board[pos.1 as usize][pos.0 as usize] = '.';
+        board[new_pos.1 as usize][new_pos.0 as usize] = symbol;
+        return;
+    }
+
+    if dir.1 == 0 {
+        step_2(board, &new_pos, dir);
+    } else {
+        let additional_pos = if board[new_pos.1 as usize][new_pos.0 as usize] == '[' {
+            (new_pos.0 + 1, new_pos.1)
+        } else {
+            (new_pos.0 - 1, new_pos.1)
+        };
+
+        step_2(board, &new_pos, dir);
+        step_2(board, &additional_pos, dir);
+    }
+
+    board[pos.1 as usize][pos.0 as usize] = '.';
+    board[new_pos.1 as usize][new_pos.0 as usize] = symbol;
+}
+
+fn print_board(board: &Vec<Vec<char>>, pos: (isize, isize)) {
+    let mut new_board = board.clone();
+    new_board[pos.1 as usize][pos.0 as usize] = '@';
+    println!(
+        "{}\n",
+        new_board.iter().map(|x| x.iter().join("")).join("\n")
+    );
 }
